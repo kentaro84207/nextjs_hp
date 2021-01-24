7e5703ee334cd8257e7c924699a19301fb499ce3
Kentaro Suzuki <kentaro84207@gmail.com>
2021-01-24 17:38:58 +0900

    Add Layout

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import Head from "next/head";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div className="min-h-screen py-0 px-2 flex flex-col justify-center items-center">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Hello Next.js</h1>
-    </div>
+    <Layout title="Home">
+      <p className="text-4xl">Hello Next.js</p>
+    </Layout>
   );
 }
